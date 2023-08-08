030676ce646836c46830b624d6f3dc2ffe44f65b
Nina Nitin Kamble <108800240+Ninanitin@users.noreply.github.com>
2023-08-08 21:18:32 +0530

    Add files via upload

---
+++ Searce/react-logging-start/App.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+//import logo from './logo.svg';
+import './App.css';
+
+/*
+function App() {
+  return <h2>Hi, my name is Joe!</h2>;
+}
+*/
+class App extends React.Component 
+{
+  render() {
+    return <h2>Hi again from Joe!</h2>;
+  }
+}
+
+/*
+function App() {
+  console.log('Hello!');
+  console.warn('Careful there!');
+  console.error('Oh, no!');
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}*/
+
+export default App;
